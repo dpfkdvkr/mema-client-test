41e617932121f82d787da884d803aafc4cbb9bed
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2024-11-29 12:16:45 +0900

    remove: 테스트코드삭제 -#2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
 'use client';
-import Button from '@/components/Button';
 
 export default function Home() {
-  const onClick = () => {
-    console.log('a');
-  };
-  return (
-    <div>
-      <Button name="버튼" onClick={onClick} />
-    </div>
-  );
+  return <div>next</div>;
 }
