7e18f6b177d933440fa3b3d5ef16758415bb34f6
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2024-12-05 22:59:57 +0900

    feat: 미팅명 변경하기 모달추가 -#14

--- src/features/meet/management/ManagementChangeName/ManagementChangeName.tsx
+++ src/features/meet/management/ManagementChangeName/ManagementChangeName.tsx
@@ -2,12 +2,16 @@ import Button from '@/components/Button';
 import Label from '@/components/common/Label';
 import Input from '@/components/Input';
 import InputWrapper from '@/components/Input/InputWrapper';
+import Modal from '@/components/Modal';
+import { Text } from '@/components/Modal/modalTypography';
 import { useInputState } from '@/hooks/useInputState';
+import useToggle from '@/lib/hooks/useToggle';
 import React from 'react';
 import styled from 'styled-components';
 
 function ManagementChangeName() {
   const name = useInputState();
+  const [isOpenModal, toggleOpenModal] = useToggle();
 
   return (
     <>
@@ -23,7 +27,12 @@ function ManagementChangeName() {
           onBlur={name.handleBlur}
         />
       </InputWrapper>
-      <StyledButton name="미팅명 변경하기" />
+      <StyledButton name="미팅명 변경하기" onClick={toggleOpenModal} />
+      {isOpenModal && (
+        <Modal type="Ok" onOk={toggleOpenModal} width={326}>
+          <Text>미팅명이 변경되었습니다.</Text>
+        </Modal>
+      )}
     </>
   );
 }
