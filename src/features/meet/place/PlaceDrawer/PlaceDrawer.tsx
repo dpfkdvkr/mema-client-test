54030cb38013724769576ddddf7f044c489f1ea6
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2024-12-24 16:32:40 +0900

    feat: AI추천맛집 UI -#97

--- src/features/meet/place/PlaceDrawer/PlaceDrawer.tsx
+++ src/features/meet/place/PlaceDrawer/PlaceDrawer.tsx
@@ -1,9 +1,11 @@
 'use client';
 import Button from '@/components/Button';
+import useToggle from '@/lib/hooks/useToggle';
 import { Station, TotalLocation } from '@/types/locate';
 import { useParams, useRouter } from 'next/navigation';
 import React from 'react';
 import styled from 'styled-components';
+import PlaceDrawerModal from './PlaceDrawerModal';
 
 type Props = {
   myLocation: string;
@@ -14,6 +16,8 @@ const PlaceDrawer = ({ myLocation, totalLocation }: Props) => {
   const router = useRouter();
   const params = useParams();
   const meetId = (params?.id && Number(params.id)) || null;
+  const [isOpenAIDrawer, toggleOpenAIDrawer] = useToggle();
+  const [isOpenAIModal, toggleOpenAIModal] = useToggle();
 
   return (
     <Container>
@@ -34,24 +38,56 @@ const PlaceDrawer = ({ myLocation, totalLocation }: Props) => {
         {/* 2명이상일떄 */}
         {totalLocation && (
           <>
-            <div className="drawer">
-              <div className="drawerTitle">
-                <b>{totalLocation.midStation.stationName}</b>까지
-                <br />
-                평균 이동 시간은 <b>15분</b>입니다!
-              </div>
-              <div className="descriptionContainer">
-                {totalLocation.startStationList.map((station: Station, index: number) => (
-                  <p key={index} className="description">
-                    <div className="number">{index + 1}</div> <b>{station.stationName}</b>에서부터{' '}
-                    <b>18분</b>
-                  </p>
-                ))}
-              </div>
-            </div>
-            <StyledButton name="추천지 보기" onClick={() => alert('준비중입니다')} />
+            {isOpenAIDrawer ? (
+              <>
+                <div className="drawer">
+                  <div className="drawerTitle">
+                    <b>{totalLocation.midStation.stationName}</b>근처의
+                    <br />
+                    먹거리, 놀거리 알려드릴게요!
+                  </div>
+                  <div className="storeContainer">
+                    <div className="store" onClick={toggleOpenAIModal}>
+                      <div className="storeImg">img</div>
+                      <div className="storeContent">
+                        <p className="storeTitle">제목</p>
+                        <p className="storeAddress">내용내용내용내용내용내용내용내용내용내용내용</p>
+                      </div>
+                    </div>
+                    <div className="store">
+                      <div className="storeImg">img</div>
+                      <div className="storeContent">
+                        <p className="storeTitle">제목</p>
+                        <p className="storeAddress">내용</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <StyledButton name="이동 시간 보기" onClick={toggleOpenAIDrawer} />
+              </>
+            ) : (
+              <>
+                <div className="drawer">
+                  <div className="drawerTitle">
+                    <b>{totalLocation.midStation.stationName}</b>까지
+                    <br />
+                    평균 이동 시간은 <b>15분</b>입니다!
+                  </div>
+                  <div className="descriptionContainer">
+                    {totalLocation.startStationList.map((station: Station, index: number) => (
+                      <p key={index} className="description">
+                        <div className="number">{index + 1}</div> <b>{station.stationName}</b>
+                        에서부터 <b>18분</b>
+                      </p>
+                    ))}
+                  </div>
+                </div>
+                <StyledButton name="추천지 보기" onClick={toggleOpenAIDrawer} />
+              </>
+            )}
           </>
         )}
+        {isOpenAIModal && <PlaceDrawerModal onClose={toggleOpenAIModal} />}
       </>
     </Container>
   );
@@ -119,6 +155,44 @@ const Container = styled.div`
         b {
           font-weight: normal;
           color: ${({ theme }) => theme.colors.green};
+        }
+      }
+    }
+    .storeContainer {
+      display: flex;
+      flex-direction: column;
+      gap: 12px;
+      .store {
+        background-color: ${({ theme }) => theme.colors.gray[6]};
+        border-radius: 15px;
+        padding: 16px 18px;
+        display: flex;
+        align-items: center;
+        gap: 12px;
+        cursor: pointer;
+        .storeImg {
+          border-radius: 15px;
+          background-color: ${({ theme }) => theme.colors.gray[4]};
+          width: 74px;
+          height: 74px;
+          flex-shrink: 0;
+          text-indent: -9999px;
+        }
+        .storeContent {
+          display: flex;
+          flex-direction: column;
+          gap: 12px;
+          width: calc(100% - 90px);
+          .storeTitle {
+            ${({ theme }) => theme.fonts.title['xs']};
+          }
+          .storeAddress {
+            ${({ theme }) => theme.fonts.text['lg']};
+            color: ${({ theme }) => theme.colors.gray[3]};
+            overflow: hidden;
+            white-space: nowrap;
+            text-overflow: ellipsis;
+          }
         }
       }
     }
---
+++ src/features/meet/place/PlaceDrawer/PlaceDrawerModal/PlaceDrawerModal.tsx
@@ -0,0 +1,139 @@
+import React from 'react';
+import { Clock, Map, Phone, Star } from 'react-feather';
+import styled from 'styled-components';
+
+type Props = {
+  onClose: () => void;
+};
+
+const PlaceDrawerModal = ({ onClose }: Props) => {
+  return (
+    <Container>
+      <div className="background" onClick={onClose}></div>
+      <div className="contentWrapper">
+        <div className="content">
+          <div className="modalImg">img</div>
+          <div className="modalContent">
+            <div className="top">
+              <p className="titleContainer">
+                <b>cafe쿰베오</b>카페, 디저트
+              </p>
+              <div className="scoreContainer">
+                <Star />
+                <p>4.86</p>
+              </div>
+            </div>
+            <div className="middle">
+              <div className="infoContent">
+                <Map />
+                <div>서울 강동구 풍성로35길 10 1층</div>
+              </div>
+              <div className="infoContent">
+                <Phone />
+                <div>0507-1406-5280</div>
+              </div>
+              <div className="infoContent">
+                <Clock />
+                <div>매일 12:00 - 22:00</div>
+              </div>
+            </div>
+            <p className="intro">소개글</p>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 390px;
+  height: 100%;
+  @media (max-width: 390px) {
+    width: 100%;
+  }
+  .background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    width: 100%;
+    height: 100%;
+    z-index: 5;
+  }
+  .contentWrapper {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
+    z-index: 6;
+    width: 326px;
+    border-radius: 15px;
+    .content {
+      display: flex;
+      flex-direction: column;
+      .modalImg {
+        width: 100%;
+        height: 180px;
+        background-color: #eee;
+        border-radius: 15px 15px 0 0;
+        text-indent: -9999px;
+      }
+      .modalContent {
+        padding: 20px 16px;
+        .top {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          margin-bottom: 24px;
+          .titleContainer {
+            ${({ theme }) => theme.fonts.text['md']};
+            color: ${({ theme }) => theme.colors.gray[4]};
+            b {
+              ${({ theme }) => theme.fonts.title['sm']};
+              color: black;
+              margin-right: 4px;
+            }
+          }
+          .scoreContainer {
+            display: flex;
+            align-items: center;
+            ${({ theme }) => theme.fonts.text['md']};
+            color: ${({ theme }) => theme.colors.primary.default};
+            svg {
+              color: #ff3434;
+              fill: #ff3434;
+              width: 16px;
+            }
+          }
+        }
+        .middle {
+          margin-bottom: 24px;
+          .infoContent {
+            display: flex;
+            align-items: center;
+            gap: 4px;
+            svg {
+              color: ${({ theme }) => theme.colors.gray[5]};
+              width: 18px;
+            }
+            div {
+              color: ${({ theme }) => theme.colors.gray[3]};
+              ${({ theme }) => theme.fonts.text['lg']};
+            }
+          }
+        }
+        .intro {
+          color: ${({ theme }) => theme.colors.gray[1]};
+          ${({ theme }) => theme.fonts.text['lg']};
+        }
+      }
+    }
+  }
+`;
+
+export default PlaceDrawerModal;
